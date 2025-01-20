4dde7942714fa92c51ec2196e522099531d52d78
Jeffrey Desir <8616589+desirtechnology@users.noreply.github.com>
2025-01-20 01:38:18 +0000

    correct: Next Config

--- app/next.config.ts
+++ app/next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  typescript: {
+
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    unoptimized: true,
+    remotePatterns: [
+      {
+        hostname: 'images.unsplash.com',
+      },
+    ]
+  }
 };
 
 export default nextConfig;
